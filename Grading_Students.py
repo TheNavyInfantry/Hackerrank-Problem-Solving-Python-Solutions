166931e35d290f6473045e319e6beb500f47f252
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-07-29 21:28:31 +0300

    Update Solution of Grading_Students.py

--- Grading_Students.py
+++ Grading_Students.py
@@ -1,11 +1,9 @@
 def gradingStudents(grades):
     for each in grades:
         if each >= 38:
-            if each % 5 == 2:
-                each += 3
-            elif each % 5 == 3:
+            if (each % 5) == 3:
                 each += 2
-            elif each % 5 == 4:
+            elif (each % 5) == 4:
                 each += 1
         print(each)
 
