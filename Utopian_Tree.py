7b86a6dc543f559533447f8104c4ea0303f49ec4
Nedim Can Ulusoy <nedimcanulusoy@gmail.com>
2020-08-03 13:22:38 +0300

    Solution of Utopian_Tree.py

---
+++ Utopian_Tree.py
@@ -0,0 +1,11 @@
+def utopianTree(n):
+    startingHeight = 1
+    for i in range(1,n+1):
+        if i % 2 != 0:
+            startingHeight *= 2
+        else:
+            startingHeight += 1
+
+    return startingHeight
+
+#odd result is spring, means *2 ; even result is summer, means +1
